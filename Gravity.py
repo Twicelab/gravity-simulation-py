ec4e9a271dbc39bc993d64888ee36a66b069fef0
Tretyakov Inniokenty <kent@vplicei.net>
2017-12-13 20:02:55 +0300

    Added new features
    Tail drawing and speed manipulating

--- Gravity.py
+++ Gravity.py
@@ -22,29 +22,44 @@ def progress(x):
 
 def endProgress():
 	global xpro
-	sys.stdout.write("#" * (20 - xpro) + "\n")
+	sys.stdout.write("#" * int(20 - xpro) + "\n")
 	sys.stdout.flush()
 
 def dist(x1,x2,y1,y2):
 	return (x1-x2)**2+(y1-y2)**2
 
 def gravity(dots,k,t,a):
+	dots1 = dots.copy()
+	dots2 = dots1.copy()
+	dots3 = dots2.copy()
+	dots4 = dots3.copy()
 	deltax=[0]*len(dots)
 	deltay=[0]*len(dots)
 	startProgress("Progress")
 	k1 = k
-	with imageio.get_writer('Gravity.gif', mode='I') as writer:
-		for u in range(0,t):
-			progress(int(100*u/t))
+	kwargs={ 'duration': 0.04 }
+	with imageio.get_writer('Gravity.gif', mode='I', **kwargs ) as writer:
+		timer = 0
+		while k1>=1:
+			progress(100 * (128-k1) / 128)
 			plt.title('Gravity Simulation')
 			plt.xlabel('First Axis')
 			plt.ylabel('Second Axis')
 			plt.axis([0, 1, 0, 1])
+			plt.text(0.05, 0.95, 'Force fading coefficient: '+str(k1),fontsize=10)
+			plt.plot([dots4[0],dots3[0]],[dots4[1],dots3[1]],c=(0.25,0.0,0.0,1.0),lw=1.0)
+			plt.plot([dots3[0],dots2[0]],[dots3[1],dots2[1]],c=(0.5,0.0,0.0,1.0),lw=2.0)
+			plt.plot([dots2[0],dots1[0]],[dots2[1],dots1[1]],c=(0.75,0.0,0.0,1.0),lw=3.0)
+			plt.plot([dots1[0],dots[0]],[dots1[1],dots[1]],c=(1.0,0.0,0.0,1.0),lw=4.0)
 			plt.plot(dots[0],dots[1],'w.',mec='r',mew=1.0,ms=8.0)
 			plt.savefig('temp.png')
 			plt.close()
 			image = imageio.imread('temp.png')
 			writer.append_data(image)
+			dots4 = dots3.copy()
+			dots3 = dots2.copy()
+			dots2 = dots1.copy()
+			dots1 = dots.copy()
 			for i in range(0,len(dots)):
 				corr = (0.5+dist(dots[0][:],dots[0][i],dots[1][:],dots[1][i]))
 				corr = corr**k1
@@ -52,10 +67,14 @@ def gravity(dots,k,t,a):
 				deltay[i] = a*(np.sum(dots[1][:]/corr)/np.sum(1/corr)-dots[1][i])+(1-a)*deltay[i]
 				dots[0][i] = dots[0][i]+deltax[i]
 				dots[1][i] = dots[1][i]+deltay[i]
-			if (t-u)>(t/5):
-				k1=int(k*(1.0 - (u+1.0)/(t-(t/5)+1)))
+			if (max(deltax)<0.005 and max(deltay)<0.005 and min(deltax)>-0.005  and min(deltay)>-0.005):
+				if (timer<25):
+					timer = timer+1
+				else:
+					k1=k1/2
+					timer = 0
 			else:
-				k1=0
+				timer = 0
 	endProgress()
 
 def GO():
