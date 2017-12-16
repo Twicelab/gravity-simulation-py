5327fa0936bc87a08b5be960d1e75b0759c11488
Tretyakov Inniokenty <kent@vplicei.net>
2017-12-16 05:07:31 +0300

    Made it more phisically adequate

--- Gravity.py
+++ Gravity.py
@@ -25,40 +25,42 @@ def endProgress():
 	sys.stdout.write("#" * int(20 - xpro) + "\n")
 	sys.stdout.flush()
 
-def dist(x1,x2,y1,y2):
-	return (x1-x2)**2+(y1-y2)**2
+def dist(x1,x2,y1,y2,z1,z2):
+	return np.power((x1-x2)**2+(y1-y2)**2+(z1-z2)**2,0.5)
 
-def gravity(dots,k,t,a,b):
+def gravity(dots,k,t,a,b,m):
 	dots1 = dots.copy()
 	dots2 = dots1.copy()
 	dots3 = dots2.copy()
 	dots4 = dots3.copy()
-	deltax=[0]*len(dots)
-	deltay=[0]*len(dots)
+	deltax=(0.5-dots[1])*(np.random.rand(len(dots)))/10
+	deltay=(dots[0]-0.5)*(np.random.rand(len(dots)))/10
+	deltaz=(np.random.rand(len(dots))-0.5)/10
 	startProgress("Progress")
 	k1 = k
 	kwargs={ 'duration': 0.04 }
 	with imageio.get_writer('Gravity.gif', mode='I', **kwargs ) as writer:
 		timer = 0
-		while k1>=1:
-			progress(100 * (128-k1) / 128)
+		while timer<500:
+			progress(100 * (500-timer) / 500)
 			plt.style.use('dark_background')
 			plt.title('Gravity Simulation')
 			plt.xlabel('First Axis')
 			plt.ylabel('Second Axis')
-			plt.axis('off')
+			plt.axis('equal')
 			plt.xlim(0, 1)
 			plt.ylim(0, 1)
-			plt.text(0.27, -0.05, 'Timelapse acceleration: 1e+'+str(64.0*k/k1),fontsize=10)
+			plt.axis('off')
+			plt.text(0.2, -0.05, '['+'|'*int(50 * (timer) / 500) + ' '*int(50 * (500-timer) / 500)+ ']',fontsize=10)
 			plt.plot([dots4[0],dots3[0]],[dots4[1],dots3[1]],c=(1.0,1.0,0.8,0.7),lw=0.5)
 			plt.plot([dots3[0],dots2[0]],[dots3[1],dots2[1]],c=(1.0,1.0,0.8,0.8),lw=1.0)
 			plt.plot([dots2[0],dots1[0]],[dots2[1],dots1[1]],c=(1.0,1.0,0.8,0.9),lw=1.5)
 			plt.plot([dots1[0],dots[0]],[dots1[1],dots[1]],c=(1.0,1.0,0.8,1.0),lw=2.0)
-			plt.plot(dots[0],dots[1],'.',c=(1.0,1.0,0.8,0.01),mew=1.0,ms=48.0)
-			plt.plot(dots[0],dots[1],'.',c=(1.0,1.0,0.8,0.05),mew=1.0,ms=24.0)
-			plt.plot(dots[0],dots[1],'.',c=(1.0,1.0,0.8,0.2),mew=1.0,ms=12.0)
-			plt.plot(dots[0],dots[1],'.',c=(1.0,1.0,0.8,0.8),mew=1.0,ms=6.0)
-			plt.plot(dots[0],dots[1],'.',c=(1.0,1.0,0.8),mew=1.0,ms=3.0)
+			plt.plot(dots[0],dots[1],'.',c=(1.0,1.0,0.8,0.01),ms=48.0)
+			plt.plot(dots[0],dots[1],'.',c=(1.0,1.0,0.8,0.05),ms=24.0)
+			plt.plot(dots[0],dots[1],'.',c=(1.0,1.0,0.8,0.2),ms=12.0)
+			plt.plot(dots[0],dots[1],'.',c=(1.0,1.0,0.8,0.8),ms=6.0)
+			plt.plot(dots[0],dots[1],'.',c=(1.0,1.0,0.8),ms=3.0)
 			plt.savefig('temp.png')
 			plt.close()
 			image = imageio.imread('temp.png')
@@ -68,24 +70,23 @@ def gravity(dots,k,t,a,b):
 			dots2 = dots1.copy()
 			dots1 = dots.copy()
 			for i in range(0,len(dots)):
-				corr = (0.5+dist(dots[0][:],dots[0][i],dots[1][:],dots[1][i]))
-				corr = corr**k1
-				deltax[i] = a*(np.sum((dots[0][:]-dots[0][i])/corr)/np.sum(1/corr)) + b*deltax[i]
-				deltay[i] = a*(np.sum((dots[1][:]-dots[1][i])/corr)/np.sum(1/corr)) + b*deltay[i]
-				dots[0][i] = dots[0][i]+deltax[i]
-				dots[1][i] = dots[1][i]+deltay[i]
-			if (k1>2):
-				k1=k1*0.99
-			else:
-				k1=k1-0.01
+				corr = (0.5+t*dist(dots[0][:],dots[0][i],dots[1][:],dots[1][i],dots[2][:],dots[2][i]))
+				deltax[i] = a*(np.sum(m*m*(dots[0][:]-dots[0][i])/corr/corr/corr))/m + b*deltax[i]
+				deltay[i] = a*(np.sum(m*m*(dots[1][:]-dots[1][i])/corr/corr/corr))/m + b*deltay[i]
+				deltaz[i] = a*(np.sum(m*m*(dots[2][:]-dots[2][i])/corr/corr/corr))/m + b*deltaz[i]
+			dots[0] = dots[0][:]+deltax[:]
+			dots[1] = dots[1][:]+deltay[:]
+			dots[2] = dots[2][:]+deltaz[:]
+			timer=timer+1
+
 	endProgress()
 
 def GO():
 	dots=[]
 	for i in range(0,100):
-		dots.append([random.random(),random.random()])
+		dots.append([random.random(),random.random(),random.random()])
 	dots=pd.DataFrame(dots)
 	print("=== Simulating gravitational force ===")
-	a=gravity(dots,128,100,0.01,0.98)
+	a=gravity(dots,2,100,0.4,0.9,30)
 
 GO()
